bb499e544644fa4a3be239ba47276bec0f27bf75
sigmarius <aamokeeva@yandex.ru>
2020-06-15 00:05:10 +0300

    Настраивает модальное окно и выбор цветов для Мага

--- js/setup.js
+++ js/setup.js
@@ -1,9 +1,16 @@
 'use strict';
 
-var FIRST_NAMES = ['Иван', 'Хуан', 'Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var LAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var FIRST_NAMES = [
+  'Иван', 'Хуан', 'Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'
+];
+var LAST_NAMES = [
+  'да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'
+];
+var COAT_COLORS = [
+  'rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'
+];
 var EYE_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 var WIZARD_COUNT = 4;
 
 var userDialog = document.querySelector('.setup');
@@ -11,6 +18,11 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template')
     .content
     .querySelector('.setup-similar-item');
 var similarListElements = userDialog.querySelector('.setup-similar-list');
+
+var setupOpen = document.querySelector('.setup-open');
+var setupIcon = document.querySelector('.setup-open-icon');
+var setupClose = userDialog.querySelector('.setup-close');
+var setupUserInput = userDialog.querySelector('.setup-user-name');
 
 var generateRandomIndex = function (min, max) {
   min = Math.ceil(min);
@@ -57,7 +69,82 @@ var renderWizards = function (arrWizards) {
 
 var wizards = generateWizards();
 
-similarListElements.appendChild(renderWizards(wizards));
+var openPopup = function () {
+  userDialog.classList.remove('hidden');
 
-userDialog.classList.remove('hidden');
+  document.addEventListener('keydown', escapeKeyHandler);
+};
+
+var closePopup = function () {
+  userDialog.classList.add('hidden');
+
+  document.removeEventListener('keydown', escapeKeyHandler);
+};
+
+var escapeKeyHandler = function (evt) {
+  if (evt.key === 'Escape' && evt.target !== setupUserInput) {
+    evt.preventDefault();
+    closePopup();
+  }
+};
+
+var getUserWizard = function () {
+  var userWizard = {
+    coat: userDialog.querySelector('.setup-wizard .wizard-coat'),
+    coatInput: userDialog.querySelector('input[name="coat-color"]'),
+    eyes: userDialog.querySelector('.setup-wizard .wizard-eyes'),
+    eyesInput: userDialog.querySelector('input[name="eyes-color"]'),
+    fireball: userDialog.querySelector('.setup-fireball-wrap'),
+    fireballInput: userDialog.querySelector('input[name="fireball-color"]')
+  };
+  return userWizard;
+};
+
+var userWizard = getUserWizard();
+
+var wizardClickHandler = function (element, input, arr) {
+  element.style.fill = getRandomElement(arr);
+  input.value = element.style.fill;
+};
+
+
+var fireballClickHandler = function () {
+  var color = getRandomElement(FIREBALL_COLORS);
+  userWizard.fireball.style.backgroundColor = color;
+  userWizard.fireballInput.value = color;
+};
+
+setupOpen.addEventListener('click', function () {
+  openPopup();
+});
+
+setupClose.addEventListener('click', function () {
+  closePopup();
+});
+
+setupClose.addEventListener('keydown', function (evt) {
+  if (evt.key === 'Enter') {
+    closePopup();
+  }
+});
+
+setupIcon.addEventListener('keydown', function (evt) {
+  if (evt.key === 'Enter') {
+    openPopup();
+  }
+});
+
+userWizard.coat.addEventListener('click', function () {
+  wizardClickHandler(userWizard.coat, userWizard.coatInput, COAT_COLORS);
+});
+
+userWizard.eyes.addEventListener('click', function () {
+  wizardClickHandler(userWizard.eyes, userWizard.eyesInput, EYE_COLORS);
+});
+
+userWizard.fireball.addEventListener('click', function () {
+  fireballClickHandler();
+});
+
+similarListElements.appendChild(renderWizards(wizards));
 userDialog.querySelector('.setup-similar').classList.remove('hidden');
